4c834abd83456840e9ccd55644d75a54add17bbf
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-11-03 11:43:21 +0600

    container optimized

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,5 +1,5 @@
 export default function Container({ children }) {
-    return <div className="max-w-5xl mx-auto">
+    return <div className="max-w-5xl mx-auto px-8 md:px-0">
         {children}
     </div>
 }
